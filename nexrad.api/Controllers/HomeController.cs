155676485af642dd1546c2844fda8fc9c5b11c2b
Mike Upjohn <michael.upjohn@hotmail.co.uk>
2019-05-04 20:43:58 +0100

    NRDR-6 Added links to projects for calling services

--- nexrad.api/Controllers/HomeController.cs
+++ nexrad.api/Controllers/HomeController.cs
@@ -1,13 +1,22 @@
-﻿using System.Web.Http;
+﻿using nexrad.reader.Level2;
+using System.Web.Http;
 
 namespace nexrad.api.Controllers
 {
     [RoutePrefix("api/v1/nexrad")]
     public class HomeController : ApiController
     {
-        [Route("high-res")]
-        public IHttpActionResult GetHighResReflectivity()
+        private readonly ILevel2RadarReader _level2RadarReader;
+
+        public HomeController(ILevel2RadarReader level2RadarReader)
         {
+            _level2RadarReader = level2RadarReader;
+        }
+
+        [Route("high-res")]
+        public IHttpActionResult GetHighResReflectivity(string fileName)
+        {
+            var data = _level2RadarReader.RunLevel2Radar("F:// " + fileName);
             return Ok(true);
         }
     }
