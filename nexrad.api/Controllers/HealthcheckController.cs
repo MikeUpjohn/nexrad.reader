7b70be8282227512e6c579a4f568a880d6b1ca65
Mike Upjohn <michael.upjohn@hotmail.co.uk>
2022-10-08 09:39:31 +0100

    11 Am I Alive endpoint.

---
+++ nexrad.api/Controllers/HealthcheckController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Http;
+
+namespace nexrad.api.Controllers
+{
+    [RoutePrefix("api/v1")]
+    public class HealthcheckController : ApiController
+    {
+        public HealthcheckController()
+        {
+        }
+
+        [HttpGet]
+        [Route("am-i-alive")]
+        public IHttpActionResult AmIAlive()
+        {
+            return Ok(new { AmIAlive = true, Date = DateTime.Now.ToString("ddd dd MMM yyyy HH:mm:ss.FFFFFF") });
+        }
+    }
+}
