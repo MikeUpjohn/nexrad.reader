d71a85d0d19033d039c751f20f9e38ad05c571a6
Mike Upjohn <michael.upjohn@hotmail.co.uk>
2025-12-20 19:16:08 +0000

    197 Front end autofac tweaks

--- nexrad.web/App_Start/WebApiConfig.cs
+++ nexrad.web/App_Start/WebApiConfig.cs
@@ -1,20 +1,15 @@
 ﻿using System.Reflection;
-using System.Web.Http;
 using System.Web.Mvc;
 using Autofac;
 using Autofac.Integration.Mvc;
-using Autofac.Integration.WebApi;
-using nexrad.api.Controllers;
 using nexrad.reader.Level2;
 using nexrad.reader.Level2.IndividualMessages;
-using nexrad.web.Controllers;
 
 namespace nexrad.web.App_Start {
     public static class WebApiConfig {
         public static void Register() {
             var builder = new ContainerBuilder();
             builder.RegisterControllers(Assembly.GetExecutingAssembly());
-            //builder.RegisterType<HomeController>().InstancePerRequest();
             builder.RegisterType<Level2RadarReader>().As<ILevel2RadarReader>().InstancePerRequest();
             builder.RegisterType<Level2RecordReader>().As<ILevel2RecordReader>().InstancePerRequest();
             builder.RegisterType<Message31Reader>().As<IMessage31Reader>().InstancePerRequest();
@@ -24,7 +19,6 @@ namespace nexrad.web.App_Start {
             builder.RegisterType<Level2MessageReader>().As<ILevel2MessageReader>().InstancePerRequest();
 
             var container = builder.Build();
-            //config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
         }
     }
