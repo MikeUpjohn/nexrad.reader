971ec5c2a4929d5f3f79629cf1081af8796640a7
Mike Upjohn <michael.upjohn@hotmail.co.uk>
2026-01-25 09:29:18 +0000

    20 Splitting files into namespaced files

---
+++ nexrad.web/Content/js/nexradviewer/nexrad.reader.js
@@ -0,0 +1,56 @@
+﻿nexrad.reader = (function () {
+    let loadFileButton;
+
+    const selectors = {
+        radarFile: '#radar-file',
+        runRadarLoop: '#run-radar-loop'
+    };
+
+    const init = () => {
+        loadFileButton = document.querySelector(selectors.runRadarLoop);
+
+        setupLoadFileHandler();
+    };
+
+    const setupLoadFileHandler = () => {
+        const loadFileButton = document.querySelector(selectors.runRadarLoop);
+        if (!loadFileButton) {
+            return;
+        }
+
+        loadFileButton.removeEventListener('click', handleLoadFile);
+        loadFileButton.addEventListener('click', handleLoadFile);
+    };
+
+    const handleLoadFile = async () => {
+        loadFileButton.setAttribute('disabled', 'disabled');
+
+        const selectedMenuItem = document.querySelector(selectors.radarFile).value;
+        if (!selectedMenuItem) {
+            return;
+        }
+
+        message = `Retrieving and loading file data for ${selectedMenuItem}`;
+        nexrad.ui.updateToastMessage(message);
+
+        const request = {
+            'RadarFile': 'KTLX20130520_200356_V06',
+            'ElevationNumber': 3
+        };
+
+        // Always required to run
+        const azimuthData = await nexrad.renderer.azimuth.loadAzimuthData(request);
+
+        // this will change later to render something based off the dropdown list of options (reflectivity, velocity etc.)
+        const reflectivityData = await nexrad.renderer.reflectivity.loadReflectivityData(request);
+
+        nexrad.renderer.reflectivity.drawReflectivity(reflectivityData, azimuthData);
+
+        nexrad.ui.disableElement(loadFileButton);
+    };
+
+    return {
+        init,
+        loadFileButton
+    };
+})();
---
+++ nexrad.web/Content/js/nexradviewer/nexrad.ui.js
@@ -0,0 +1,54 @@
+﻿nexrad.ui = (function () {
+    let bootstrapToast;
+    let toast;
+
+    const selectors = {
+        toastBody: '.toast-body',
+        toastMessage: '#toast-message'
+    };
+
+    const init = () => {
+        toast = document.querySelector(selectors.toastMessage);
+        bootstrapToast = new bootstrap.Toast(toast);
+        message = '';
+
+        scene = new THREE.Scene();
+        renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight - 56);
+        cameraPosition = 100;
+        camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+        document.body.append(renderer.domElement);
+
+        renderScene();
+    };
+
+    const updateToastMessage = message => {
+        if (!bootstrapToast) {
+            return;
+        }
+
+        bootstrapToast._element.querySelector(selectors.toastBody).innerText = message;
+        bootstrapToast.show();
+    };
+
+    const disableElement = element => {
+        if (!element) {
+            return;
+        }
+
+        element.setAttribute('disabled', 'disabled');
+    }
+
+    const renderScene = () => {
+        requestAnimationFrame(renderScene);
+        camera.position.z = cameraPosition;
+        renderer.render(scene, camera);
+    };
+
+    return {
+        init,
+        updateToastMessage,
+        disableElement
+    };
+})();
